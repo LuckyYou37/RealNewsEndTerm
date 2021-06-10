f0212199eb7a06f7c8304b51c78cc0c887d1c41e
babootanguy <75026465+babootanguy@users.noreply.github.com>
2021-06-11 02:20:40 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,54 @@
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const https = require('https');
+//const mongoose = require('mongoose');
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
 
+let city = 'Nur-Sultan';
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.get('/', function(req, res){
+    const url = "https://api.openweathermap.org/data/2.5/weather?appid=07e6be11058f287f345016199616cd77&q=" + city +"&units=metric";
+    https.get(url, function(response) {
+        response.on("data", function(data) {
+            const weatherData = JSON.parse(data);
+            res.setHeader('Content-Type', 'text/html');
+            let today = new Date();
+            let dd = String(today.getDate()).padStart(2, '0');
+            let mm = today.getMonth() + 1;
+            let month = '';
+            switch (mm) {
+                case 1: month = 'JAN'; break;
+                case 2: month = 'FEB'; break;
+                case 3: month = 'MAR'; break;
+                case 4: month = 'APR'; break;
+                case 5: month = 'MAY'; break;
+                case 6: month = 'JUN'; break;
+                case 7: month = 'JUL'; break;
+                case 8: month = 'AUG'; break;
+                case 9: month = 'SEP'; break;
+                case 10: month = 'OCT'; break;
+                case 11: month = 'NOV'; break;
+                case 12: month = 'DEC';
+            }
+            today = dd + " " + month;
+            res.render('index', {description: weatherData.weather[0].main, temperature: Math.round(weatherData.main.temp), city: city, date: today});
+        })
+    })
 })
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/public/assets/style.css');
+})
+
+
+
+/*
+mongoose.connect("mongodb://localhost:27017/newsdb", {useNewUrlParser: true,useUnifiedTopology: true});
 
 const newsSchema = new mongoose.Schema({
     title: String,
@@ -19,34 +59,10 @@ const newsSchema = new mongoose.Schema({
 });
 
 const News = new mongoose.model("News", newsSchema);
-
-/* const news = new News({
-    title: "khovansky",
-    date: "2021-06-09",
-    category: "sci-fi"
-});
-
-const news1 = {
-
-    title:"kek",
-    category: "kek1",
-}
-
-News.insertMany([news,news1],function(err,foundItems){
-    if(err){
-        console.log(err);
-    } else{
-        foundItems.forEach(function(element){
-            console.log(element.title);
-        })
-        console.log(foundItems);
-    }
-});
 */
-
-// news.save()
-
 
 app.listen( 27017, function() {
     console.log("http://localhost:21017")
 })
+
+
